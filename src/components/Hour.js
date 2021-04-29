2d526195819f0bc05c0312ac7e106ef6306812eb
Oskar Findahl <oskar_findahl@hotmail.com>
2021-04-29 18:18:25 +0200

    add weather icons

--- src/components/Hour.js
+++ src/components/Hour.js
@@ -3,14 +3,17 @@ import Moment from "react-moment";
 const Hour = ({ data, tempFSet }) => {
   return (
     <div className="weatherbox-small">
-      <Moment className="text2" format="ddd HH">
-        {data.dt * 1000}
-      </Moment>
-      <div className="text2">
+      <h3>
+        <Moment className="text2" format="ddd HH">
+          {data.dt * 1000}
+        </Moment>
+      </h3>
+
+      <h3>
         {tempFSet === "false"
           ? `${Math.round(data.temp)}\xB0C`
           : `${Math.round(data.temp * 1.8 + 32)}\xB0F`}
-      </div>
+      </h3>
 
       <div className="icon">
         <img
--- src/components/Now.js
+++ src/components/Now.js
@@ -4,31 +4,35 @@ import Moment from "react-moment";
 const Now = ({ data, tempFSet }) => {
   return (
     <div className="weatherbox">
-      <Moment className="text1" format="HH:ddd">
-        {data.dt * 1000}
-      </Moment>
+      <h2>
+        <Moment format="HH ddd">{data.dt * 1000}</Moment>
+      </h2>
 
-      <div className="text1">
+      <h2>
         {tempFSet === "false"
           ? `${Math.round(data.temp)}\xB0C`
           : `${Math.round(data.temp * 1.8 + 32)}\xB0F`}
-      </div>
-
+      </h2>
       <div className="icon">
         <img
-          className="weather-icon"
           src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
           alt="weather icon"
         />
       </div>
-      <div className="text2">
-        Humidity: {data.humidity} %
-        <br />
-        Wind: {data.wind_speed} m/s
-        <br />
-        Sunrise: <Moment format="H:mm">{data.sunrise * 1000}</Moment>
-        <br />
-        Sunset: <Moment format="H:mm ">{data.sunset * 1000}</Moment>
+
+      <div className="dataContainer">
+        <img className="icon2" src="images/humidity.png" alt="wind icon" />
+        <h3>{data.humidity}%</h3>
+        <img className="icon2" src="images/wind.png" alt="wind icon" />
+        <h3>{data.wind_speed}m/s</h3>
+        <img className="icon2" src="images/sunrise.png" alt="wind icon" />
+        <h3>
+          <Moment format="H:mm">{data.sunrise * 1000}</Moment>{" "}
+        </h3>
+        <img className="icon2" src="images/sunset.png" alt="wind icon" />
+        <h3>
+          <Moment format="H:mm ">{data.sunset * 1000}</Moment>
+        </h3>
       </div>
     </div>
   );
